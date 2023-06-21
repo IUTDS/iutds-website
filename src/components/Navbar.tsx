a037d19cba555ee0bdc2f6a20557d43bd696e6e3
Sajid Chowdhury <sajidahmed@iut-dhaka.edu>
2023-06-22 02:17:25 +0600

    redesigned navbar and made it responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,45 +1,72 @@
+"use client"
+
 import Link from "next/link";
+import { Menu, X } from 'feather-icons-react';
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(true);
+    
+    const navLinks = document.querySelector('.nav-links');
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+        navLinks?.classList.toggle('top-[9%]');
+    };
+
+
     return (
-        <nav className="bg-gray-900 py-4">
-            <div className="container mx-auto flex justify-between items-center">
-                <div className="flex items-center">
-                    <div className="mr-2">
-                        <img
-                            src="/logo.png"
-                            alt="IUTDS Logo"
-                            className="h-8 w-8"
-                        />
-                    </div>
-                    <Link href="/">
-                        <div>
-                            <h1 className="text-white font-bold text-lg">IUTDS</h1>
-                        </div>
-                    </Link>
-                </div>
-                <div className="flex ml-4 mr-4 pl-10 pr-10">
-                    <Link className="mr-4" href="/about">
-                        About
-                    </Link>
-                    <Link className="mr-4" href="/panel">
-                        Panel
-                    </Link>
-                    <Link className="mr-4" href="/programs">
-                        Events
-                    </Link>
-                    <Link className="mr-4" href="/achievements">
-                        Achievements
-                    </Link>
-                    <Link className="mr-4" href="/blog">
-                        Blog
-                    </Link>
-                    <Link className="mr-4" href="/debate-resources">
-                        Debate Resources
-                    </Link>
+        <div className="bg-[#000000]">
+        <nav className="flex justify-between items-center w-[92%] mx-auto ">
+            <div>
+                <img className="w-16 text-white" src="../assets/images/IUTDS_Logo.svg" alt="IUTDS logo" />
+            </div>
+
+            <div className="nav-links duration-500 
+                            md:static absolute bg-white 
+                            md:min-h-fit min-h-[60vh] left-0 
+                            top-[-100%] md:w-auto  w-full flex items-center px-5
+                            bg-[#000000]"
+            >
+                <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
+                    <li>
+                        <Link href="\about" className="hover:text-gray-500 text-white">
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="\achievements" className="hover:text-gray-500 text-white">
+                            Achievements
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="\blog" className="hover:text-gray-500 text-white">
+                            Blogs
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="\panel" className="hover:text-gray-500 text-white">
+                            Panel
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="\programs" className="hover:text-gray-500 text-white">
+                            Events
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="flex items-center gap-6">
+                <div className="text-3xl cursor-pointer md:hidden" onClick={toggleMenu}>
+                    {isMenuOpen ? (
+                        <Menu color="white"/>
+                    ) : (
+                        <X color="white"/>
+                    )}
                 </div>
             </div>
+
         </nav>
+       </div>
     );
 };
 
