bece128d9a7d5a4c6ca2fa575871f2687e9ea7e8
Sajid Chowdhury <sajidahmed@iut-dhaka.edu>
2023-08-10 02:01:36 +0600

    blog page design completed

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,11 +1,213 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from "next/link";
+
+import {Book} from 'feather-icons-react';
+
 
 const page = () => {
   return (
-    <section className="w-full flex-center flex-col">
-        <h1 className="head_text text-center red_gradient">Blog</h1>
+    <section className="container mx-auto py-6">
+        <h1 className="head_text text-center red_gradient mb-4">Our Blogs</h1>
+
+        <div className='grid grid-cols-3 gap-14 mb-8'>
+          
+          <div className='shadow-xl rounded-xl'>
+            <Image 
+                className="rounded center-image"
+                width={385} height={300} 
+                src="/blog-images/artOfSpeaking.jpeg" 
+                alt="President, IUTDS" 
+            />
+            
+            <h3 className='ml-4 mr-10 my-4 font-semibold'>
+              Mastering the Art of Speaking to captivate and conquer
+            </h3>
+            
+            <div className='horizontal-line my-4 ml-4'></div>
+            
+            <p className='ml-4 mr-10 my-4'>
+            Speaking is a skill that one must hone to ease through most of their life, be it in a 
+            professional setting or personal matters. There are two aspects to speaking...
+            </p>
+            
+            <div className='ml-4 mr-10 my-4 flex flex-row'>
+              <Book size={24} />
+              <h3 className='text-gray-500 ml-0.5'>Sajid Chowdhury</h3>
+              <Link href='/blog/art-of-speaking'>
+                <button className='ml-20 bg-black text-white py-1 px-2 rounded shadow hover:bg-gray-700 transition duration-300'>
+                  Read more
+                </button>
+              </Link>
+              
+            </div>
+            
+          </div>
+
+          <div className='shadow-xl rounded-xl'>
+            <Image 
+                className="rounded center-image"
+                width={385} height={300} 
+                src="/blog-images/artOfSpeaking.jpeg" 
+                alt="President, IUTDS" 
+            />
+            
+            <h3 className='ml-4 mr-10 my-4 font-semibold'>
+              Mastering the Art of Speaking to captivate and conquer
+            </h3>
+            
+            <div className='horizontal-line my-4 ml-4'></div>
+            
+            <p className='ml-4 mr-10 my-4'>
+            Speaking is a skill that one must hone to ease through most of their life, be it in a 
+            professional setting or personal matters. There are two aspects to speaking...
+            </p>
+            
+            <div className='ml-4 mr-10 my-4 flex flex-row'>
+              <Book size={24} />
+              <h3 className='text-gray-500 ml-0.5'>Sajid Chowdhury</h3>
+              <Link href='/blog/art-of-speaking'>
+                <button className='ml-20 bg-black text-white py-1 px-2 rounded shadow hover:bg-gray-700 transition duration-300'>
+                  Read more
+                </button>
+              </Link>
+              
+            </div>
+            
+          </div>
+
+          <div className='shadow-xl rounded-xl'>
+            <Image 
+                className="rounded center-image"
+                width={385} height={300} 
+                src="/blog-images/artOfSpeaking.jpeg" 
+                alt="President, IUTDS" 
+            />
+            
+            <h3 className='ml-4 mr-10 my-4 font-semibold'>
+              Mastering the Art of Speaking to captivate and conquer
+            </h3>
+            
+            <div className='horizontal-line my-4 ml-4'></div>
+            
+            <p className='ml-4 mr-10 my-4'>
+            Speaking is a skill that one must hone to ease through most of their life, be it in a 
+            professional setting or personal matters. There are two aspects to speaking...
+            </p>
+            
+            <div className='ml-4 mr-10 my-4 flex flex-row'>
+              <Book size={24} />
+              <h3 className='text-gray-500 ml-0.5'>Sajid Chowdhury</h3>
+              <Link href='/blog/art-of-speaking'>
+                <button className='ml-20 bg-black text-white py-1 px-2 rounded shadow hover:bg-gray-700 transition duration-300'>
+                  Read more
+                </button>
+              </Link>
+              
+            </div>
+            
+          </div>
+
+          <div className='shadow-xl rounded-xl'>
+            <Image 
+                className="rounded center-image"
+                width={385} height={300} 
+                src="/blog-images/artOfSpeaking.jpeg" 
+                alt="President, IUTDS" 
+            />
+            
+            <h3 className='ml-4 mr-10 my-4 font-semibold'>
+              Mastering the Art of Speaking to captivate and conquer
+            </h3>
+            
+            <div className='horizontal-line my-4 ml-4'></div>
+            
+            <p className='ml-4 mr-10 my-4'>
+            Speaking is a skill that one must hone to ease through most of their life, be it in a 
+            professional setting or personal matters. There are two aspects to speaking...
+            </p>
+            
+            <div className='ml-4 mr-10 my-4 flex flex-row'>
+              <Book size={24} />
+              <h3 className='text-gray-500 ml-0.5'>Sajid Chowdhury</h3>
+              <Link href='/blog/art-of-speaking'>
+                <button className='ml-20 bg-black text-white py-1 px-2 rounded shadow hover:bg-gray-700 transition duration-300'>
+                  Read more
+                </button>
+              </Link>
+              
+            </div>
+            
+          </div>
+
+          <div className='shadow-xl rounded-xl'>
+            <Image 
+                className="rounded center-image"
+                width={385} height={300} 
+                src="/blog-images/artOfSpeaking.jpeg" 
+                alt="President, IUTDS" 
+            />
+            
+            <h3 className='ml-4 mr-10 my-4 font-semibold'>
+              Mastering the Art of Speaking to captivate and conquer
+            </h3>
+            
+            <div className='horizontal-line my-4 ml-4'></div>
+            
+            <p className='ml-4 mr-10 my-4'>
+            Speaking is a skill that one must hone to ease through most of their life, be it in a 
+            professional setting or personal matters. There are two aspects to speaking...
+            </p>
+            
+            <div className='ml-4 mr-10 my-4 flex flex-row'>
+              <Book size={24} />
+              <h3 className='text-gray-500 ml-0.5'>Sajid Chowdhury</h3>
+              <Link href='/blog/art-of-speaking'>
+                <button className='ml-20 bg-black text-white py-1 px-2 rounded shadow hover:bg-gray-700 transition duration-300'>
+                  Read more
+                </button>
+              </Link>
+              
+            </div>
+            
+          </div>
+
+          <div className='shadow-xl rounded-xl'>
+            <Image 
+                className="rounded center-image"
+                width={385} height={300} 
+                src="/blog-images/artOfSpeaking.jpeg" 
+                alt="President, IUTDS" 
+            />
+            
+            <h3 className='ml-4 mr-10 my-4 font-semibold'>
+              Mastering the Art of Speaking to captivate and conquer
+            </h3>
+            
+            <div className='horizontal-line my-4 ml-4'></div>
+            
+            <p className='ml-4 mr-10 my-4'>
+            Speaking is a skill that one must hone to ease through most of their life, be it in a 
+            professional setting or personal matters. There are two aspects to speaking...
+            </p>
+            
+            <div className='ml-4 mr-10 my-4 flex flex-row'>
+              <Book size={24} />
+              <h3 className='text-gray-500 ml-0.5'>Sajid Chowdhury</h3>
+              <Link href='/blog/art-of-speaking'>
+                <button className='ml-20 bg-black text-white py-1 px-2 rounded shadow hover:bg-gray-700 transition duration-300'>
+                  Read more
+                </button>
+              </Link>
+              
+            </div>
+            
+          </div>
+
+        </div>
     </section>
   )
 }
+
 
 export default page
