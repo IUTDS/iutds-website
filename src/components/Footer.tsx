434588ec5cbf48f5932cae1ddfb0ba7f2f5adbc0
ssshanto <ssshanto@gmail.com>
2023-08-20 19:52:12 +0600

    add website

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="bg-black md:rounded-tl-full md:rounded-tr-full rounded-tl-3xl rounded-tr-3xl border p-12 text-center">
       <div className="flex flex-col items-center">
-        <div className="flex gap-10 text-white pb-5">
+        <div className="flex gap-10 text-white pb-2">
           <Link href="https://www.facebook.com/iutdebatingsociety">
             <BsFacebook size={25} className="hover:text-red-500" />
           </Link>
@@ -17,6 +17,13 @@ const Footer = () => {
             <BsLinkedin size={25} className="hover:text-red-500" />
           </Link>
         </div>
+        <Link
+          href="iutds@iut-dhaka.edu"
+          className="md:text-lg pb-5 text-sm text-slate-300 lowercase
+          hover:brightness-200 hover:text-white"
+        >
+          iutds@iut-dhaka.edu
+        </Link>
         <p className="md:text-xl text-lg text-white tracking-widest font-light uppercase">
           IUT Debating Society
         </p>
